03adc1c9e875209bbd7209b280327f150e08bd90
PRoldanL <roldan.linares.pedro.antonio@gmail.com>
2023-12-11 00:09:05 -0600

    Esta es la primera entrega de repositorios para el corte del lunes 11 de diciembre de 2023

---
+++ Javascript/11.js
@@ -0,0 +1,61 @@
+//Comentarios de una sola linea
+
+/* Comentarios
+multilinea*/
+
+//alert ("hola mundo");
+
+//console.log("Este es un mensaje impreso en la consola.");
+
+
+/* var nombre = prompt("Ingresa tu nombre");
+alert("Hola, "+ nombre + " Bienvenido a la CH35");*/
+
+/* var numero1 = parseInt(prompt("Ingresa el primer numero"));
+   var numero2 = parseInt(prompt("Ingresa el segundo numero"));
+
+console.log("suma:" + (numero1 + numero2));
+console.log("resta:" + (numero1 - numero2));
+console.log("multiplicacion:" + (numero1 * numero2));
+console.log("division:" + (numero1 / numero2)); */
+
+var edad = parseInt(prompt("Ingresa tu edad:"));
+
+if (edad >= 18) {
+    alert("Bienvenido a Somos Poker. ¡Disfruta de la página!");
+} else {
+    alert("Lo siento, debes ser mayor de 18 años para acceder a Somos Poker. Acceso denegado.");
+}
+
+
+
+//Tipo de datos y variables
+
+//scope es el alcance de nuestras variables
+
+/* La diferencia entre var, let y const es el alcance:
+
+var tiene un alcance global
+let tiene un alcance local
+const como su nombre lo dice se mantiene constante */
+
+/*string:cadena de texto (lleva "" comillas)
+number: numero
+noolena: true/false
+null:nulo
+undefinied:no esta asigando o definido*/
+
+//Programa de invitados 
+
+//Definiendo variables
+
+// let nombre = "Daniel";
+// let invitadosExtra = 2;
+// let esMayorEdad = true;
+// let edad = 24;
+// let invitadoEspecial = null
+// let horaDeSalida = undefined;
+
+
+
+
